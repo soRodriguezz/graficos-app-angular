4a180881e632d8a7b00bc9b2e6771d4408d61f14
soRodriguezz <s.rodriguezzapata01@gmail.com>
2021-11-07 12:55:54 -0300

    grafica barra

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'graficas', 
+    loadChildren: () => import('./graficas/graficas-routing.module').then(m => m.GraficasRoutingModule)
+  },
+  {
+    path: '**', redirectTo: 'graficas'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
